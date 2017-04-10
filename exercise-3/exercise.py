379f6585d145200cb3a59ca364c0dfd8b68172c6
WANG CHEN <wangc79@uw.edu>
2017-04-10 00:14:41 -0700

    Complete exercise 3

--- exercise-3/exercise.py
+++ exercise-3/exercise.py
@@ -16,6 +16,35 @@
 #
 # Be sure to consider both positive AND negative distances!
 # BONUS: Also print out whether the guess is high or low
+import random
+
+def high_or_low(target, guess):
+    if target > guess:
+        print("the guess is low")
+    if target < guess:
+        print("the guess is high")
+
+def print_hot_or_cold(target, guess):
+    if target == guess:
+        print("got it!")
+    elif abs(guess - target) <= 1:
+        print("scalding hot")
+        high_or_low(target, guess)
+    elif abs(guess - target) <= 3:
+        print("very warm")
+        high_or_low(target, guess)
+    elif abs(guess - target) <= 5:
+        print("warm")
+        high_or_low(target, guess)
+    elif abs(guess - target) <= 8:
+        print("cold")
+        high_or_low(target, guess)
+    elif abs(guess - target) <= 13:
+        print("very cold")
+        high_or_low(target, guess)
+    else:
+        print("icy freezing miserably cold")
+        high_or_low(target, guess)
 
 
 
@@ -28,9 +57,30 @@
 # make MULTIPLE guesses. You can either do this using a loop (see the next module)
 # or by simply calling your `guess_number() method again IF the user didn't get
 # the answer right. This is an example of **recursion**.
-
+def guess_number(target):
+    print("Welcome to Guess_Number! Guess a number between 1-100. If you want to quit, enter 'quit' anytime")
+    guess = input("Please enter a number to make a guess --> ")
+    if guess == "quit":
+        print("Goodbye~")
+        return 
+    guess = float(guess)
+    count = 1
+    while guess != target:
+        print_hot_or_cold(target,guess)
+        guess = input("Please make another guess --> ")
+        if guess == "quit":
+            print("Goodbye~")
+            return
+        guess = float(guess) 
+        count += 1
+    print("Congrats! You got it using %d trials!" %count)
+    
 
 
 # If the file is run as a top-level script, your script should pick a random number
 # between 1 and 50 as the target and then start the game. You should inform the
 # use of the range of numbers before asking them for a guess.
+
+
+if __name__ == '__main__':
+    guess_number(random.randint(1,100))
